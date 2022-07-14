1af54d071b93420fe0e0ac61200083a18820ea91
HFrost0 <2498393441@qq.com>
2022-07-14 23:21:37 +0800

    use class to reconstruct server

--- echo/echo_server_threadpool.py
+++ echo/echo_server_threadpool.py
@@ -1,10 +1,9 @@
-import socket
 from concurrent.futures import ThreadPoolExecutor
 
-from echo.base_server import BaseServer
+from echo.echo_server_thread import ThreadServer
 
 
-class ThreadPoolServer(BaseServer):
+class ThreadPoolServer(ThreadServer):
     def __init__(self, address=("127.0.0.1", 6666)):
         super(ThreadPoolServer, self).__init__(address)
         self.pool = ThreadPoolExecutor()
@@ -14,14 +13,6 @@ class ThreadPoolServer(BaseServer):
             s, addr = self.listener.accept()
             print(f'Connection: {addr}')
             self.pool.submit(self.handler, s)
-
-    def handler(self, sock: socket.socket):
-        while data := sock.recv(1024):
-            sock.send(data)
-            # sum(range(100000))  # slow down respond
-            print(f'Echo: {data}')
-        sock.close()
-        print(f'Remove: {sock}')
 
 
 if __name__ == '__main__':
